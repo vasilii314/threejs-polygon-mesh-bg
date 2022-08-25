1c95784fa95242980d56fbd2e8f747e6231fd33f
vasilii314 <pythagoras3145926@gmail.com>
2022-08-25 12:00:09 +0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,177 @@
+import * as THREE from 'https://unpkg.com/three@0.133.1/build/three.module.js';
+import { OrbitControls } from "https://cdn.skypack.dev/three@0.133.1/examples/jsm/controls/OrbitControls";
+import dat from "https://cdn.skypack.dev/dat.gui";
+import gsap from 'https://cdn.skypack.dev/gsap'
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer();
+const raycaster = new THREE.Raycaster();
+
+const gui = new dat.GUI();
+const world = {
+    plane: {
+        width: 400,
+        height: 400,
+        widthSegments: 30,
+        heightSegments: 30,
+    }
+};
+
+function generatePlane() {
+    planeMesh.geometry.dispose();
+    planeMesh.geometry = new THREE.PlaneGeometry(
+        world.plane.width,
+        world.plane.height,
+        world.plane.widthSegments,
+        world.plane.heightSegments);
+        const { array } = planeMesh.geometry.attributes.position;
+        const randomValues = [];
+        
+        for (let i = 0; i < array.length; i ++) {
+            if (i % 3 === 0) {
+                const x = array[i];
+                const y = array[i + 1];
+                const z = array[i + 2];
+            
+                array[i] = x + (Math.random() - 0.5) * 10;
+                array[i + 1] = y + (Math.random() - 0.5) * 10;
+                array[i + 2] = z + Math.random() * 10;
+                
+            }
+            randomValues.push(Math.random() * Math.PI * 2);
+        }
+        
+        planeMesh.geometry.attributes.position.randomValues = randomValues;
+        
+        planeMesh.geometry.attributes.position.originalPosition = planeMesh.geometry.attributes.position.array;
+        const colors = [];
+
+        for (let i = 0; i < planeMesh.geometry.attributes.position.count; i++) {
+            colors.push(0, 0.19, 0.4);
+        }    
+        planeMesh.geometry.setAttribute(
+            'color',
+            new THREE.BufferAttribute(new 
+                Float32Array(colors), 3)
+                );
+            
+}
+gui.add(world.plane, 'width', 1, 400)
+.onChange(generatePlane);
+
+gui.add(world.plane, 'height', 1, 400)
+.onChange(generatePlane);
+
+gui.add(world.plane, 'widthSegments', 1, 100)
+.onChange(generatePlane);
+
+gui.add(world.plane, 'heightSegments', 1, 100)
+.onChange(generatePlane);
+
+console.log(scene);
+console.log(camera);
+console.log(renderer);
+
+renderer.setClearColor(new THREE.Color(0x000000));
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(devicePixelRatio);
+document.body.appendChild( renderer.domElement );
+
+const planeGeometry = new THREE.PlaneGeometry(30, 30, 27, 27);
+const planeMaterial = new THREE.MeshPhongMaterial({
+    
+    side: THREE.DoubleSide,
+    flatShading: THREE.FlatShading, 
+    vertexColors: true,
+});
+const planeMesh = new THREE.Mesh(planeGeometry, planeMaterial);
+scene.add(planeMesh);
+generatePlane();
+
+const light = new THREE.DirectionalLight(0xFFFFFF, 1);
+light.position.set(0, 1, 1);
+
+const light2 = new THREE.DirectionalLight(0xFFFFFF, 1);
+light2.position.set(0, 0, -1);
+
+scene.add(light);
+scene.add(light2);
+
+new OrbitControls(camera, renderer.domElement)
+
+camera.position.z = 50;
+
+const mouse = {
+    x: undefined,
+    y: undefined,
+};
+
+let frame = 0;
+function animate() {
+    window.requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+    const {array, originalPosition, randomValues} = planeMesh.geometry.attributes.position
+    
+    frame += 0.01;
+    for (let i = 0; i < array.length; i += 3) {
+        array[i] = originalPosition[i] + Math.cos(frame + randomValues[i]) * 0.01;
+        array[i + 1] = originalPosition[i + 1] + Math.sin(frame + randomValues[i + 1]) * 0.01;
+    }
+
+
+    planeMesh.geometry.attributes.position.needsUpdate = true;
+    
+    raycaster.setFromCamera(mouse, camera);
+    const intersects = raycaster.intersectObject(planeMesh);
+    if (intersects.length > 0) {
+        const { color } = intersects[0].object.geometry.attributes
+        color.setX(intersects[0].face.a, 0.1);
+        color.setY(intersects[0].face.a, 0.5);
+        color.setZ(intersects[0].face.a, 1);
+        color.setX(intersects[0].face.b, 0.1);
+        color.setY(intersects[0].face.b, 0.5);
+        color.setZ(intersects[0].face.b, 1);
+        color.setX(intersects[0].face.c, 0.1);
+        color.setY(intersects[0].face.c, 0.5);
+        color.setZ(intersects[0].face.c, 1);
+        color.needsUpdate = true;
+
+        const initialColor = {
+            r: 0,
+            g: 0.19,
+            b: 0.4,
+        }
+
+        const hoverColor = {
+            r: 0.1,
+            g: 0.5,
+            b: 1,
+        }
+
+        gsap.to(hoverColor, {
+            r: initialColor.r,
+            g: initialColor.g,
+            b: initialColor.b,
+            onUpdate: () => {
+                color.setX(intersects[0].face.a, hoverColor.r);
+                color.setY(intersects[0].face.a, hoverColor.g);
+                color.setZ(intersects[0].face.a, hoverColor.b);
+                color.setX(intersects[0].face.b, hoverColor.r);
+                color.setY(intersects[0].face.b, hoverColor.g);
+                color.setZ(intersects[0].face.b, hoverColor.b);
+                color.setX(intersects[0].face.c, hoverColor.r);
+                color.setY(intersects[0].face.c, hoverColor.g);
+                color.setZ(intersects[0].face.c, hoverColor.b);
+                color.needsUpdate = true;
+            }
+        });
+    }
+}
+
+animate();
+
+addEventListener('mousemove', (event) => {
+    mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+    mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+});
